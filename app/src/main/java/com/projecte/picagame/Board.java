644a1beca67cf02b30ba651a24a66ad608b32dd8
LordVader03 <101247894+LordVader03@users.noreply.github.com>
2023-02-25 14:25:41 +0100

    Corrected javadoc format

--- app/src/main/java/com/projecte/picagame/Board.java
+++ app/src/main/java/com/projecte/picagame/Board.java
@@ -8,53 +8,60 @@ import ...
  * Esta clase define el tablero del juego, que serán 3 cartas por jugador
  */
 
+/**
+ * Grid será el tablero de tamaño 3x1
+ * Y dice serán los valores que puedan salir con el dado
+ */
+
 public class Board {
-    /**
-     * Grid será el tablero de tamaño 3x1
-     * Y dice serán los valores que puedan salir con el dado
-     */
     int grid[2];
     int dice[5] = '1, 2, 3, 4, 5, 6';
 }
 
+/**
+ * Esta función simulará el hecho de añadir una carta al tablero
+ */
+
 public void add_card(){
-    /**
-     * Esta función simulará el hecho de añadir una carta al tablero
-     */
-    @TODO
+    //@TODO
 }
+
+/**
+ * Esta función mirará si el tablero es vacío
+ */
 
 public void grid_empty(){
-    /**
-     * Esta función mirará si el tablero es vacío
-     */
-    @TODO
+    //@TODO
 }
+
+/**
+ * Función que mira si está vacío el tablero
+ */
 
 public void isfull(){
-    /**
-     * Función que mira si está vacío el tablero
-     */
-    @TODO
+    //@TODO
 }
+
+/**
+ * Comprueba si la baraja de un jugador ya se ha acabado
+ */
 
 public void check_deck(){
-    /**
-     * Comprueba si la baraja de un jugador ya se ha acabado
-     */
-    @TODO
+    //@TODO
 }
+
+/**
+ * Mira las cartas que hay en el tablero
+ */
 
 public void card_in_play(){
-    /**
-     * Mira las cartas que hay en el tablero
-     */
-    @TODO
+    //@TODO
 }
 
+/**
+ * Quita la carta del tablero
+ */
+
 public void remove_card(){
-    /**
-     * Quita la carta del tablero
-     */
-    @TODO
+    //@TODO
 }
